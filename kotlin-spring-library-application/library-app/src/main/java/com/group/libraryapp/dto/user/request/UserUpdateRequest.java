efc3c38f0d043a007568b4916aa2545bba4f590f
danaRyu <dana40426@gmail.com>
2022-09-12 14:45:18 +0900

    test: add user test by kotlin

--- kotlin-spring-library-application/library-app/src/main/java/com/group/libraryapp/dto/user/request/UserUpdateRequest.java
+++ kotlin-spring-library-application/library-app/src/main/java/com/group/libraryapp/dto/user/request/UserUpdateRequest.java
@@ -1,14 +1,22 @@
 package com.group.libraryapp.dto.user.request;
 
+import org.jetbrains.annotations.NotNull;
+
 public class UserUpdateRequest {
 
   private long id;
   private String name;
 
+  public UserUpdateRequest(long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
   public long getId() {
     return id;
   }
 
+  @NotNull
   public String getName() {
     return name;
   }
--- kotlin-spring-library-application/library-app/src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ kotlin-spring-library-application/library-app/src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -1,8 +1,11 @@
 package com.group.libraryapp.service.user
 
+import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -13,6 +16,11 @@ class UserServiceTest @Autowired constructor (
     private val userRepository: UserRepository,
     private val userService: UserService
 ) {
+
+    @AfterEach
+    fun afterEach() {
+        userRepository.deleteAll()
+    }
 
     @Test
     fun saveUserTest() {
@@ -27,6 +35,44 @@ class UserServiceTest @Autowired constructor (
         assertThat(results).hasSize(1)
         assertThat(results[0].name).isEqualTo("danadot")
         assertEquals(results[0].age, null) // kotlin -> age가 nullable인지 판단 불가 (java code) --> @Nullable annotation 추가
+    }
+
+    @Test
+    fun getUsersTest() {
+        //given
+        userRepository.saveAll(listOf(User("danadot", null), User("mipong", 2)))
+
+        //when
+        val results = userService.getUsers()
+
+        //then
+        assertThat(results).hasSize(2)
+        assertThat(results).extracting("name").containsExactlyInAnyOrder("danadot", "mipong")
+    }
+
+    @Test
+    fun updateUserName() {
+        // given
+        val user = User("danadot", null)
+        val savedUser = userRepository.save(user)
+
+        // when
+        userService.updateUserName(UserUpdateRequest(savedUser.id, "meepong"))
+
+        // then
+        assertEquals(userRepository.findById(savedUser.id).get().name, "meepong")
+    }
+
+    @Test
+    fun deleteUser() {
+        //given
+        userRepository.save(User("heepong", 29))
+
+        //when
+        userService.deleteUser(User("heepong", 29).name)
+
+        //then
+        assertThat(userRepository.findByName("heepong")).isEmpty
     }
 
 }
